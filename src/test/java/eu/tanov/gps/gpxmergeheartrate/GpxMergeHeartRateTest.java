301fb4f6f6f1039d08999407fb6bbf8e8649f1a1
Plamen Tanov <1.plamen@gmail.com>
2020-08-15 22:06:32 +0300

    fixes #9 Support gpx files with <extensions>

--- src/test/java/eu/tanov/gps/gpxmergeheartrate/GpxMergeHeartRateTest.java
+++ src/test/java/eu/tanov/gps/gpxmergeheartrate/GpxMergeHeartRateTest.java
@@ -13,12 +13,11 @@ import eu.tanov.gps.gpxmergeheartrate.HeartRateProvider.Statistics;
 
 public class GpxMergeHeartRateTest {
 
-	@Test
-	public void testProcessMiBandTools() throws Exception {
+	private void assertResult(String inputGpx, String inputHeartRate, String output) throws Exception {
 		final GpxMergeHeartRate gpxMergeHeartRate = new GpxMergeHeartRate();
-		try (final InputStream gpxInputFile = getClass().getResourceAsStream("input.gpx")) {
-			try (final InputStream heartRateFile = getClass().getResourceAsStream("input-mibandtools.csv")) {
-				final String expectedResult = IOUtils.toString(getClass().getResourceAsStream("output.gpx"));
+		try (final InputStream gpxInputFile = getClass().getResourceAsStream(inputGpx)) {
+			try (final InputStream heartRateFile = getClass().getResourceAsStream(inputHeartRate)) {
+				final String expectedResult = IOUtils.toString(getClass().getResourceAsStream(output));
 
 				final ByteArrayOutputStream actualOutput = new ByteArrayOutputStream();
 
@@ -34,23 +33,17 @@ public class GpxMergeHeartRateTest {
 	}
 
 	@Test
+	public void testProcessMiBandTools() throws Exception {
+		assertResult("input.gpx", "input-mibandtools.csv", "output.gpx");
+		assertResult("inputExtensions.gpx", "input-mibandtools.csv", "outputExtensions.gpx");
+		assertResult("inputTrackPointExtension.gpx", "input-mibandtools.csv", "outputTrackPointExtension.gpx");
+	}
+
+	@Test
 	public void testProcessNotifyFitnessForMiBand() throws Exception {
-		final GpxMergeHeartRate gpxMergeHeartRate = new GpxMergeHeartRate();
-		try (final InputStream gpxInputFile = getClass().getResourceAsStream("input.gpx")) {
-			try (final InputStream heartRateFile = getClass().getResourceAsStream("input-notifyfitnessformiband.csv")) {
-				final String expectedResult = IOUtils.toString(getClass().getResourceAsStream("output.gpx"));
-
-				final ByteArrayOutputStream actualOutput = new ByteArrayOutputStream();
-
-				final Statistics actualStatistics =
-						gpxMergeHeartRate.process(gpxInputFile, heartRateFile, actualOutput, Optional.empty());
-
-				assertEquals(1, actualStatistics.getCountSucceed());
-				assertEquals(0, actualStatistics.getCountFailed());
-
-				assertEquals(expectedResult, actualOutput.toString());
-			}
-		}
+		assertResult("input.gpx", "input-notifyfitnessformiband.csv", "output.gpx");
+		assertResult("inputExtensions.gpx", "input-notifyfitnessformiband.csv", "outputExtensions.gpx");
+		assertResult("inputTrackPointExtension.gpx", "input-notifyfitnessformiband.csv", "outputTrackPointExtension.gpx");
 	}
 
 }
